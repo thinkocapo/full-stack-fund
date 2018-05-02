14ae335882961e6e1866d31854bbadc4572f9893
thinkocapo <thinkocapo@gmail.com>
2018-05-01 22:01:42 -0400

    clean, commented results

--- solidity/MasterContract.sol
+++ solidity/MasterContract.sol
@@ -2,11 +2,16 @@ contract Lottery {
     uint public etherContribution;
     uint public maxPlayers;
 
-    address[] public players;
+    address[] public activePlayers;
 
     function Lottery (uint _etherContribution, uint _maxPlayers) public payable {
         etherContribution = _etherContribution;
         maxPlayers = _maxPlayers;
+        activePlayers.push(msg.sender);
+    }
+
+    function getMaxPlayers() public view returns (uint) {
+        return maxPlayers;
     }
 }
 
@@ -26,19 +31,23 @@ contract MasterContract {
 
 
     function createLottery(uint _etherContribution, uint _maxPlayers) public payable {
-        
         Lottery newLottery = (new Lottery).value(msg.value)(_etherContribution, _maxPlayers);
         newLotteryAddress = address(newLottery);
         lotteries.push(newLottery);
     }
 
     function getLotteries() public view returns (Lottery[]) {
-        return lotteries;
-        // > deployed.getLotteries()
-        //[ '0xa48bd859d59d451c700c19dda6c36e3e9e0d1dec' ]
+        return lotteries; // returns array of addresses [ '0xa48bd859d59d451c700c19dda6c36e3e9e0d1dec' ]
     }
     function getNewLotteryAddress() public view returns (address) {
         return newLotteryAddress;
+    }
+    function getLottery() public view returns (Lottery) { // pass by arg? functionality
+        return Lottery(newLotteryAddress); // returns an address, not the obj....why? solidity seems to not like returning entire objects...
+    }
+    function getData() public view returns (uint) {
+        Lottery lottery = Lottery(newLotteryAddress); 
+        return lottery.getMaxPlayers(); // returns { [String: '5'] s: 1, e: 0, c: [ 5 ] }
     }
     
 }
