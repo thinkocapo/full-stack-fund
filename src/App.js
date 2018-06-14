801f8d573647a1d867feae9cfb7a4f99b5c5bcce
thinkocapo <thinkocapo@gmail.com>
2018-06-14 17:40:24 -0400

    centered

--- src/App.js
+++ src/App.js
@@ -14,10 +14,24 @@ import Links from './components/links'
 //   message: React.PropTypes.string
 // }
 
+
 class App extends Component {
   render() {
     return (
       <div className="container">
+        <div className="row" style={{height: "300px"}}></div>
+
+        <div className="row">
+          <div className="col-sm-6 num-players">
+            <input type="numplayers" className="form-control right-side input-width pull-right" id="numplayers" placeholder="number of players"></input>
+          </div>
+          <div className="col-sm-6 bet">
+            <input type="bet" className="form-control input-width" id="bet" placeholder="ether bet"></input>
+          </div>
+        </div>
+
+        <div className="row" style={{height: "10px"}}></div>
+
         <div className="row">
           <div className="center-me">
             <button type="button" className="btn btn-primary btn-lg">GAMBLE</button>
