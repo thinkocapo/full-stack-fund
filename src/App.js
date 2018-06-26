a99a7f962d6877893ba3ba2e11834fbebdf91425
thinkocapo <thinkocapo@gmail.com>
2018-06-26 16:27:05 +0300

    eth lib restructuring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux'
-import Ethereum from './lib/ethereum'
+import Ethereum from './lib/ethereum/connection'
 // import { placeBet } from './services/ethereum' // export defualt function mymethod () { }
 
 
