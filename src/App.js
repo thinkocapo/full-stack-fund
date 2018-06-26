b21be1e0869cccd3fc5e4fe6adf7510a59e15bd2
thinkocapo <thinkocapo@gmail.com>
2018-06-26 14:12:44 +0300

    ether bet, numPlayers set in front-end State

--- src/App.js
+++ src/App.js
@@ -15,8 +15,30 @@ class App extends Component {
   // MasterContract was already deployed by setup-ethereum-react.js, no need for end user to do this every time they load this webpage
   constructor () {
     super()
+    this.state = {
+      etherBet: '',
+      numPlayers: ''
+    }
     this.ethereum = new Ethereum() // methods only for interacting with existing contracts
-    
+    this.placeBet = this.placeBet.bind(this)
+    this.setEtherBet = this.setEtherBet.bind(this)
+    this.setNumPlayers = this.setNumPlayers.bind(this)
+  }
+
+  placeBet(domEvent) {
+    const { etherBet, numPlayers } = this.state
+    console.log('GAMBLE...', { etherBet, numPlayers })
+  }
+
+  // TODO validation of user inputs
+  // onClick()
+  setEtherBet(domEvent) {
+    const ether = domEvent.target.value
+    this.state.etherBet = ether
+  }
+  setNumPlayers(domEvent) {
+    const numPlayers = domEvent.target.value
+    this.state.numPlayers = numPlayers
   }
 
   render() {
@@ -25,11 +47,11 @@ class App extends Component {
         <div className="row" style={{height: "300px"}}></div>
 
         <div className="row">
-          <div className="col-sm-6 num-players">
-            <input type="numplayers" className="form-control right-side input-width pull-right" id="numplayers" placeholder="number of players"></input>
-          </div>
           <div className="col-sm-6 bet">
-            <input type="bet" className="form-control input-width" id="bet" placeholder="ether bet"></input>
+            <input onChange={this.setEtherBet} name="etherBet" placeholder="ether bet amount" type="bet" className="form-control right-side input-width pull-right" id="bet" ></input>
+          </div>
+          <div className="col-sm-6 num-players">
+            <input onChange={this.setNumPlayers} name="numPlayers" placeholder="number of players" type="numplayers" className="form-control input-width" id="numplayers"></input>
           </div>
         </div>
 
@@ -37,7 +59,7 @@ class App extends Component {
 
         <div className="row">
           <div className="center-me">
-            <button type="button" className="btn btn-primary btn-lg">GAMBLE</button>
+            <button onClick={this.placeBet} type="button" className="btn btn-primary btn-lg">GAMBLE</button>
           </div>
         </div>      
       </div>
